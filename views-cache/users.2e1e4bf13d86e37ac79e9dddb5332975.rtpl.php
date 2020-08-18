6bb96afba1f53bddd065b37759c8672720f32fac
Alexandre Tampieri <tampieri@bol.com.br>
2020-08-17 22:32:45 -0300

    CRUD Admin Categorias

--- views-cache/users.2e1e4bf13d86e37ac79e9dddb5332975.rtpl.php
+++ views-cache/users.2e1e4bf13d86e37ac79e9dddb5332975.rtpl.php
@@ -19,7 +19,7 @@
   		<div class="box box-primary">
             
             <div class="box-header">
-              <a href="/admin/users/create" class="btn btn-success">Cadastrar Usuário</a>
+              <a href="/admin/users/create" class="btn btn-success"><i class="fa fa-user-plus"></i> Cadastrar Usuário</a>
             </div>
 
             <div class="box-body no-padding">
